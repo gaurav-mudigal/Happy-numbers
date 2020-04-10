18191b1e496e7e3d7b310e42c04ec5f77dcbcf5b
Gaurav Mudigal <mudigalg@amazon.com>
2020-04-10 15:02:43 +0530

    Update happy.js

--- happy.js
+++ happy.js
@@ -1,6 +1,6 @@
 var isHappy = function (n) {
     while (n <= 50) {
-        let str = n.toString().split("").reduce((acc, ele) => acc += Math.pow(ele), 0);
+        let str = n.toString().split("").reduce((acc, ele) => acc += Math.pow(ele, 2), 0);
 
         if (str === 1) {
             return true;
